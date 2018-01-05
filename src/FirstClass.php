dd8e956a8367d1be57c52e66a453db3a1d30ab96
爹 <155166348@qq.com>
2018-01-05 16:09:20 +0800

    初始化

---
+++ src/FirstClass.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 18/1/5
+ * Time: 下午4:06
+ */
+namespace First;
+
+class FirstClass
+{
+    public function xxx()
+    {
+        echo 'xxx';
+    }
+}
